616aae2159f0aba1a419c219a52e8757ef447d43
Harrison Hsieh <harrison@withglide.com>
2024-02-24 16:19:13 -0500

    telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
-"nvim-lua/plenary.nvim",
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  "nvim-lua/plenary.nvim",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    end
+  }
 }
